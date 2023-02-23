5a3c82a474f5689eb0ccc33a9f21bca5d9a57190
juanpuerto23 <juan2224511@correo.uis.edu.co>
2023-02-23 08:11:10 -0500

    Quiz

---
+++ Calculadora/Ejecutable/Test.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+public class CalculadoraBasica
+{
+    public static void main(String[] args)
+    {
+        // Declaración de variables
+        double x, y;
+        int opcion;
+        boolean bandera;
+
+        // Entrada
+        JOptionPane.showMessageDialog(null, "CALCULADORA BÁSICA\n\n1. Ingrese los valores para x e y.\n2. Seleccione la operación a realizar");
+        x = Double.parseDouble(JOptionPane.showInputDialog("Digite el valor de x: "));
+        y = Double.parseDouble(JOptionPane.showInputDialog("Digite el valor de y: "));
+        opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "OPERACIONES:\n\n1. Suma.\n2. Resta\n3. Multiplicacion\n4. División\n5. Potencia\n6. Logaritmo"));
+
+        // Procesamiento
+        switch(opcion)
+        {
+            case 1:
+                // Suma
+                JOptionPane.showMessageDialog(null, "La suma es: "+  (x+y));
+                break;
+            case 2:
+                // Resta
+                JOptionPane.showMessageDialog(null, "La resta es: " + (x-y));
+                break;
+            default:
+                //Otros casos
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+        }
+
+        // Salida
+
+        // Fin
+        System.exit(0);
+    }
+}
---
+++ Quiz_1/Ejecutable/Test.java
@@ -0,0 +1,64 @@
+package Ejecutable;
+
+import  javax.swing.JOptionPane;
+
+public class Test
+{
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null, "Escriba un numero de un digito", "De numero a letras", JOptionPane.INFORMATION_MESSAGE);
+
+        int numero;
+
+        numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el número de un digito: "));
+
+        switch (numero) 
+        {
+            case 1:
+                JOptionPane.showMessageDialog(null, "Uno", "1", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            
+            case 2:
+                JOptionPane.showMessageDialog(null, "Dos", "2", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case 3:
+                JOptionPane.showMessageDialog(null, "Tres", "3", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case 4:
+                JOptionPane.showMessageDialog(null, "Cuatro", "4", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case 5:
+                JOptionPane.showMessageDialog(null, "Cinco", "5", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case 6:
+                JOptionPane.showMessageDialog(null, "Seis", "6", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            
+            case 7:
+                JOptionPane.showMessageDialog(null, "Siete", "7", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case 8:
+                JOptionPane.showMessageDialog(null, "Ocho", "8", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case 9:
+                JOptionPane.showMessageDialog(null, "Nueve", "9", JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case 0:
+                JOptionPane.showMessageDialog(null, "Cero", "0", JOptionPane.INFORMATION_MESSAGE);
+                break;
+        }
+
+        if (numero < 0 & numero >= 10)
+        {
+            JOptionPane.showMessageDialog(null, "El numero no está entre el rango indicado", null, numero);
+        }
+
+    }
+}
