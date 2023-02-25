cdb4a058c63a31198de9e57cfc6d643242421500
juanpuerto23 <juan2224511@correo.uis.edu.co>
2023-02-25 09:36:50 -0500

    Subiendo ejercicio faltante

--- Taller_1/Ejercicio2/Ejecutable/Test.java
+++ Taller_1/Ejercicio2/Ejecutable/Test.java
@@ -1,9 +1,24 @@
 package Ejercicio2.Ejecutable;
 
+import javax.swing.JOptionPane;
+
 public class Test 
 {
     public static void main(String[] args) 
     {
-        
-    }    
+        int x;
+        double fact = 1;
+
+        x = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero a calcular: "));
+
+        if(x > 0)
+        {
+            for(int i = 1; i  <= x; i++)
+            {
+                fact = fact * i;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "El factorial del numero " + x + " es: " + fact);
+    }
 }
