ce5d6fe6a49703186b474320f1c99dc241e5da06
juanpuerto23 <juan2224511@correo.uis.edu.co>
2023-03-02 00:23:44 -0500

    Mejorando el codigo

--- ArregloCalorias/Ejecutable/Ejecutable/Test.java
+++ ArregloCalorias/Ejecutable/Ejecutable/Test.java
@@ -2,23 +2,53 @@ package Ejecutable;
 
 import javax.swing.JOptionPane;
 
-public class Test
+public class Test 
 {
     public static void main(String[] args) 
     {
+        JOptionPane.showMessageDialog(null, "Por favor, introduzca las calorias diarias");
+
         int[] calorias;
-        calorias = new int[6];
+        String[] diaSemana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
 
-        calorias[0] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las calorias quemadas el lunes: "));
-        calorias[1] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las calorias quemadas el martes: "));
-        calorias[2] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las calorias quemadas el miercoles: "));
-        calorias[3] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las calorias quemadas el jueves: "));
-        calorias[4] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las calorias quemadas el viernes: "));
-        calorias[5] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las calorias quemadas el sabado: "));
-        calorias[6] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese las calorias quemadas el domingo: "));
+        calorias = new int[7];
 
-        
+        for (int i = 0; i < calorias.length; i++)
+        {
+            calorias[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de calorias consumidas el dia " + diaSemana[i] + ": ", "0"));
+        }
 
+        int opcion;
+
+        opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Eliga una opcion: \n1) Calorias de la semana \n2)Promedio de calorias \n3)Calcular el IMB (Indice de masa corporal)", "Menu Calorias", JOptionPane.DEFAULT_OPTION));
+
+        if(opcion == 1)
+        {
+            JOptionPane.showMessageDialog(null, "El numero de calorias para el dia " + diaSemana[0] + " es: " + calorias[0] + "\nEl numero de calorias para el dia " + diaSemana[1] + " es: " + calorias[1] + "\nEl numero de calorias para el dia " + diaSemana[2] + " es: " + calorias[2] + "\nEl numero de calorias para el dia " + diaSemana[3] + " es: " + calorias[3] + "\nEl numero de calorias para el dia " + diaSemana[4] + " es: " + calorias[4] + "\nEl numero de calorias para el dia " + diaSemana[5] + " es: " + calorias[5] + "\nEl numero de calorias para el dia " + diaSemana[6] + " es: " + calorias[6], "Calorias semanales", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        if(opcion == 2)
+        {
+            int promedio;
+            promedio = (calorias[0] + calorias[1] + calorias[2] + calorias[3] + calorias[4] + calorias[5] + calorias[6])/7;
+            JOptionPane.showMessageDialog(null, "El promedio de calorias en esta semana fueron de: " + promedio, "Promedio Calorias", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        if(opcion == 3)
+        {
+            double peso;
+            double altura;
+            int edad;
+            double imb;
+
+            peso = Integer.parseInt(JOptionPane.showInputDialog(null, "Por favor, ingrese su peso actual (kg)"));
+            altura = Integer.parseInt(JOptionPane.showInputDialog(null, "Por favor, ingrese su altura actual (cm)"));
+            edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Por favor, ingrese su edad actual"));
+            imb = Math.round((peso/(altura * altura)) * 10000);
+                
+            JOptionPane.showMessageDialog(null, "El indice de masa corporal a la edad de " + edad + " es igual a " + imb, "IMB", JOptionPane.CLOSED_OPTION);
+
+        }
 
     }
 }
